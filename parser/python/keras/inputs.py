103f6c838bd20f0356ae14e823318ccd41a7c5a9
Harshit Dhyani <harshitdhyani575@gmail.com>
2020-10-13 11:22:38 +0530

    Update inputs.py

--- parser/python/keras/inputs.py
+++ parser/python/keras/inputs.py
@@ -6,33 +6,34 @@ from pprint import pprint
 
 def image_input(inputs):
     """
-    req in input json -
+    request in input json.
+
     dataset-path -> path to dataset
-    image-augment -> params for image augmentation if required
-    params -> params for building generator
+    image-augment -> parameters for image augmentation if required
+    params -> parameters for building generator
     """
     base = inputs['dataset']['path']
     test_dir = os.path.join(base, 'test')
     train_dir = os.path.join(base, 'train')
-    
-    paths = \
-"""
 
-base = '{}'
-train_dir = os.path.join(base, 'train')
-test_dir = os.path.join(base, 'test')
-""".format(inputs['dataset']['path'])
+    paths = \
+        """
+        base = '{}'
+        train_dir = os.path.join(base, 'train')
+        test_dir = os.path.join(base, 'test')
+        """.format(inputs['dataset']['path'])
 
     generators = \
-"""
-augment = {}
-kwargs = {}
+        """
+        augment = {}
+        kwargs = {}
 
-train_datagen = ImageDataGenerator(**augment)
-test_datagen = ImageDataGenerator(**augment)
-train_generator = train_datagen.flow_from_directory(train_dir, **kwargs)
-test_generator = test_datagen.flow_from_directory(test_dir, **kwargs)
+        train_datagen = ImageDataGenerator(**augment)
+        test_datagen = ImageDataGenerator(**augment)
+        train_generator = train_datagen.flow_from_directory(train_dir,
+                                                            **kwargs)
+        test_generator = test_datagen.flow_from_directory(test_dir, **kwargs)
 
-""".format(inputs['image']['augment'], inputs['image']['params'])
+        """.format(inputs['image']['augment'], inputs['image']['params'])
 
     return paths+generators
