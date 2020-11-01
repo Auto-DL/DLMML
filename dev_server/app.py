bc7b87807dea1d6f1cdd03a4cb2692f61a0055eb
Halix267 <harshitdhyani1501@gmail.com>
2020-11-01 19:11:16 +0530

    changes-to-app.py

--- dev_server/app.py
+++ dev_server/app.py
@@ -1,13 +1,49 @@
-from flask import Flask
+from flask import Flask, request
+from flask import jsonify, make_response
+import json
+import os
+import importlib
+import sys
 
+sys.path.append("..")
+
+
+from utils import json_to_dict
+
+from parser import *
 
 app = Flask(__name__)
 
+@app.route('/generate/', methods=['GET', 'POST'])
+def generate(): 
+    inputs = request.get_json()
+    inputs = json_to_dict.MakeDict(inputs).parse()
+    lib = inputs.get('lib', 'keras')
+    lang = inputs.get('lang', 'python')
+    parser_path ='parser.'+lang+'.'+lib+'.main'
+    
+    parser = importlib.import_module(parser_path)
 
-@app.route('/')
-def hello():
-    """Return Hello World."""
-    return "Hello World!"
+    status, error = parser.generate_code(inputs)
+    if status:
+        print("Error", error)
+        msg = error
+        path = ''
+    else:
+        print("File generated") 
+        msg = 'File Generated Successfully'
+        path = 'file:///'+os.getcwd()+os.sep+'test.py'
+
+    response = {
+                    'message': msg, 
+                    'path': path
+                            }
+    res = make_response(jsonify(response))
+
+    return res
+    
+
+    
 
 
 if __name__ == '__main__':
